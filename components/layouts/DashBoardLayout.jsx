60fdd69967a8e83dbb5635146118738bca153219
Adrian Ramos <ramosadrian56@gmail.com>
2020-10-14 00:27:14 -0700

    fix

--- components/layouts/DashBoardLayout.jsx
+++ components/layouts/DashBoardLayout.jsx
@@ -10,7 +10,7 @@ export default function DashBoardLayout({ children }) {
                     rel="stylesheet"
                     href="https://use.fontawesome.com/releases/v5.15.1/css/all.css"
                     integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp"
-                    crossorigin="anonymous"
+                    crossOrigin="anonymous"
                 />
             </Head>
 
